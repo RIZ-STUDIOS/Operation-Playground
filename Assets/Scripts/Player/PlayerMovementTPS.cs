e04f2b0a018cbfcc5d3828d2259b49f4657f4e63
Zackaria <scehr@outlook.com>
2023-11-16 20:47:59 +0000

    New TPS movement. Amended PlayerShooter to use LateUpdate for firing as it was causing firing issues in Update. Updated .gitignore.

---
+++ Assets/Scripts/Player/PlayerMovementTPS.cs
@@ -0,0 +1,118 @@
+using OperationPlayground.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace OperationPlayground
+{
+    public class PlayerMovementTPS : MonoBehaviour
+    {
+        [SerializeField]
+        private float maxSpeed = 6;
+
+        [SerializeField]
+        private float lookSensitivity = 12;
+
+        private PlayerManager playerManager;
+
+        private CharacterController controller;
+
+        private Vector2 moveDirection;
+        private Vector3 rotateDirection;
+
+        #region Rotation Experiment
+
+        private Camera playerCamera;
+        private Transform playerTransform;
+        private OPPlayerInput.MovementActions playerMovement;
+        private float rotX;
+        private float rotY;
+
+        #endregion
+
+        private bool isLooking;
+
+        private void Awake()
+        {
+            playerManager = GetComponentInParent<PlayerManager>();
+            controller = GetComponent<CharacterController>();
+
+            playerTransform = playerManager.playerTransform;
+            playerMovement = playerManager.playerInput.Movement;
+
+            playerManager.playerInput.Movement.Move.performed += OnMovePerformed;
+            playerManager.playerInput.Movement.Move.canceled += OnMoveCanceled;
+
+            playerManager.playerInput.Movement.Look.performed += OnLookPerformed;
+            playerManager.playerInput.Movement.Look.canceled += OnLookCanceled;
+
+            playerCamera = GetComponentInChildren<Camera>();
+        }
+
+        private void Update()
+        {
+            MoveCharacter();
+
+            if (isLooking) RotateCharacter();
+        }
+
+
+        private void MoveCharacter()
+        {
+            if (!controller.enabled) return;
+            var direction = playerManager.PlayerCamera.WorldToCameraVector(moveDirection) * maxSpeed;
+
+            controller.SimpleMove(direction);
+        }
+
+        /// <summary>
+        /// Left stick moves the player relative to the camera.
+        /// </summary>
+        /// <param name="value"></param>
+        private void OnMovePerformed(InputAction.CallbackContext value)
+        {
+            moveDirection = value.ReadValue<Vector2>();
+        }
+
+        private void OnMoveCanceled(InputAction.CallbackContext value)
+        {
+            moveDirection = value.ReadValue<Vector2>();
+        }
+
+        /// <summary>
+        /// Right stick rotates the player relative to the camera.
+        /// </summary>
+        /// <param name="value"></param>
+        private void OnLookPerformed(InputAction.CallbackContext value)
+        {
+            isLooking = true;
+        }
+
+        private void OnLookCanceled(InputAction.CallbackContext value)
+        {
+            isLooking = false;
+        }
+
+        private void RotateCharacter()
+        {
+            Vector2 rotValue = playerMovement.Look.ReadValue<Vector2>();
+            
+            rotY += rotValue.x * Time.deltaTime * lookSensitivity;
+            rotX -= rotValue.y * Time.deltaTime * lookSensitivity;
+
+            rotX = Mathf.Clamp(rotX, -90f, 90f);
+
+            playerTransform.rotation = Quaternion.Euler(0, rotY, 0);
+        }
+
+        public void SetPosition(Vector3 position)
+        {
+            if (!controller) controller = GetComponentInParent<CharacterController>();
+            var enabled = controller.enabled;
+            controller.enabled = false;
+            transform.position = position;
+            controller.enabled = enabled;
+        }
+    }
+}
